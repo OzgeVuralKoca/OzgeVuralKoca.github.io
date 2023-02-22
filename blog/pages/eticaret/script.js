dfb110961cbe62606ff0d8912644b2403f166364
Your Name <you@example.com>
2023-02-22 18:00:57 +0300

    .

--- blog/pages/eticaret/script.js
+++ blog/pages/eticaret/script.js
@@ -1,57 +1,16 @@
-let products = [
-    {
-        name: "Dell Inspiron 7710",
-        image: "https://m.media-amazon.com/images/I/71O2OURn+yL._AC_SY355_.jpg",
-        price: 34.973,
-        stock: 10
-    },
-    {
-        name: "Steelseries Apex 5",
-        image: "https://m.media-amazon.com/images/I/71xVotbL3kL._AC_SX679_.jpg",
-        price: 2.026,
-        stock: 8
-    },
-    {
-        name: "Acer Aspire 7",
-        image: "https://m.media-amazon.com/images/I/71v0Qf+ih5L._AC_SX425_.jpg",
-        price: 15.699,
-        stock: 7
-    },
-    {
-        name: "MSI NB GF63 THIN 11UD-615XTR",
-        image: "https://m.media-amazon.com/images/I/71ttPN2OS3L._AC_SX425_.jpg",
-        price: 22.499,
-        stock: 40
-    },
-    {
-        name: "Fırfırlı Balon Kollu Pamuklu Bluz",
-        image: "https://cdn.dsmcdn.com/ty83/product/media/images/20210316/8/71954755/118384899/1/1_org_zoom.jpg",
-        price: 210,
-        stock: 40
-    },
-    {
-        name: "Bebek Pamuklu Kot Gömlek",
-        image: "https://cdn.dsmcdn.com/ty222/product/media/images/20211103/20/165206354/225228591/1/1_org_zoom.jpg",
-        price: 209,
-        stock: 70
-    },
-    {
-        name: "Pamuklu Kargo Pantolon",
-        image: "https://cdn.dsmcdn.com/ty24/product/media/images/20201114/13/26184850/63802247/1/1_org_zoom.jpg",
-        price: 330,
-        stock: 54
-    },
-    {
-        name: "Kız Çocuk Pamuklu Pembe Mont",
-        image: "https://cdn.dsmcdn.com/ty690/product/media/images/20230117/21/260554418/676939334/1/1_org_zoom.jpg",
-        price: 699,
-        stock: 86
-    }
-]
+let products = [];
 
 let baskets = [];
 let orders = [];
 
+const getProducts = async () => {
+    const response = await fetch("/blog/data.json");
+    const productList = await response.json();
+    products = productList;
+    showProducts();
+}
+
+getProducts()
 showProducts();
 setBasketCount();
 checkBasketCountForPaymentButton();
@@ -59,40 +18,50 @@ setOrderSpanCount();
 
 function showProducts() {
     let count = products.length;
-    let element = `<div class="row">`;
+    let electronicsElement = "";
+    let accessorryElement = ""
+    let fashionElement = ""
     for (let i = 0; i < count; i++) {
-        element += `
-        <div class="col-md-3">
-        <div class="card mb-4">
-            <img src="${products[i].image}" class="card-img-top m-auto mt-1 img-fluid" alt="product">
-            <div class="card-body">
-                <h5 class="card-title">${products[i].name}</h5>
-                <span class="card-text">Fiyat: ${products[i].price} ₺</span>
-                <p class="card-text text-bg-danger">Kalan Adet: ${products[i].stock}</p>
-                <a href="#" onclick="addBasket(${i})" class="btn btn-warning">Sepete Ekle</a>
+        let element = `
+        <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+            <div class="card mb-4">
+                <div style="min-height:250px" class="d-flex align-items-center justify-content-center">
+                    <img src="${products[i].image}" height="300px" class="card-img-top w-50 img-fluid" alt="product">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">${products[i].name}</h5>
+                    <h6 class="card-text">Category: ${products[i].category}</h6>
+                    <span class="card-text">Price: ${products[i].price} ₺</span>
+                    <p class="card-text text-bg-danger">Stock: ${products[i].stock}</p>
+                    <a href="#" onclick="addBasket(${i})" class="btn btn-warning">Add to Basket</a>
+                </div>
             </div>
         </div>
-        </div>
         `
+
+
+
+
+        if (products[i].category === "electronics") {
+            electronicsElement += element
+        } else if (products[i].category === "jewelery") {
+            accessorryElement += element
+        } else if (products[i].category === "fashion") {
+            fashionElement += element
+        }
+
+
     }
-    element += `</div>`;
 
-    let productsElement = document.getElementById("product");
-    productsElement.innerHTML = element;
+let electronicElements = document.getElementById("productElectronic");
+electronicElements.innerHTML += electronicsElement;
 
+let accessorryElements = document.getElementById("productAccessory");
+accessorryElements.innerHTML += accessorryElement
+
+let fashionElements = document.getElementById("productFashion");
+fashionElements.innerHTML += fashionElement
 }
-
-// function setBasketCount() {
-//     let element = document.getElementById("basketcount");
-//     if (baskets.length == 0) {
-//         document.getElementById("basketSpanElement").className += "hidden"
-//         element.className += "hidden"
-//     } else {
-//         document.getElementById("basketSpanElement").className = "position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger";
-//         element.className = ""
-//     }
-//     element.innerText = baskets.length
-// }
 
 function setBasketCount() {
     let element = document.getElementById("basketcount");
@@ -138,7 +107,7 @@ function setBasketModalTable() {
             <td><img src="${baskets[i].image}" width="50"</td>
             <td>${baskets[i].quantity}</td>
             <td>${baskets[i].price}</td>
-            <td>${baskets[i].price * 1}</td>
+            <td>${baskets[i].price * baskets[i].quantity}</td>
             <td>
                 <button onclick="removeBasketById(${i})" class="btn btn-outline-danger btn-sm">
                     <i class="fa fa-trash"></i>
@@ -153,7 +122,7 @@ function setBasketModalTable() {
 function removeBasketById(index) {
     let existingBasket = baskets[index];
 
-    if (existingBasket.quantity > 0) {
+    if (existingBasket.quantity > 1) {
         existingBasket.quantity--;
         let product = products.find(product => product.name === existingBasket.name);
         product.stock++;
