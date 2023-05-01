6c7b135a90f8262cc69ae908b951a7adb4e48ba5
OzgeVuralKoca <ozgevuralkoca@gmail.com>
2023-05-02 00:43:11 +0300

    .

---
+++ movie/js/main.js
@@ -0,0 +1,140 @@
+// Navbar Scroll Background
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+  header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+// Swiper Js
+var swiper = new Swiper(".home", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  hashNavigation: {
+    watchState: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// Movies
+let movies = []
+
+const getMovies = async () => {
+  const response = await fetch("data.json")
+  const movieList = await response.json()
+  movies = movieList
+  showMovies()
+}
+
+getMovies()
+
+function showMovies() {
+  let film = ""
+  let tvseries = ""
+  let fantasy = ""
+  let comedy = ""
+  let count = movies.length
+  for (let i = 0; i < count; i++) {
+    let element = `
+      <div class="box">
+        <div class="box-img">
+          <img src="${movies[i].image}" alt="">
+        </div>
+        <div class="box-info">
+          <h3>${movies[i].title}</h3>
+          <span>${movies[i].description}</span>
+          <div>
+            <p class="kind">${movies[i].kind} | ${movies[i].genres}</p>
+          </div>
+          <p style="margin-bottom:5px;">
+            Stars: <span>${movies[i].cast}</span>
+          </p>
+          <div>
+            <a href="${movies[i].trailer}" target="_blank" class="play2">
+              <i class="fa-solid fa-play"></i> Trailer
+            </a>
+          </div>
+        </div>
+      </div>
+    `
+
+    if (movies[i].kind === "movie") {
+      if (document.getElementById("films")) {
+        film += element
+      }
+    }
+
+    if (movies[i].kind === "tv series") {
+      if (document.getElementById("series")) {
+        tvseries += element
+      }
+    }
+
+    if (movies[i].genres === "Fantasy") {
+      if (document.getElementById("fantasies")) {
+        fantasy += element
+      }
+    }
+
+    if (movies[i].genres === "Comedy") {
+      if (document.getElementById("comedies")) {
+        comedy += element
+      }
+    }
+  }
+
+  let films = document.getElementById("films")
+  if (films) {
+    films.innerHTML += film
+  }
+
+  let series = document.getElementById("series")
+  if (series) {
+    series.innerHTML += tvseries
+  }
+
+  let fantasies = document.getElementById("fantasies")
+  if (fantasies) {
+    fantasies.innerHTML += fantasy
+  }
+
+  let comedies = document.getElementById("comedies")
+  if (comedies) {
+    comedies.innerHTML += comedy
+  }
+}
+
+// slider-container Arrows
+const leftArrows = document.querySelectorAll(".chevron-left");
+const rightArrows = document.querySelectorAll(".chevron-right");
+const movieLists = document.querySelectorAll(".slider-container")
+
+leftArrows.forEach((leftArrow, i) => {
+  leftArrow.addEventListener("click", function () {
+    movieLists[i].scrollLeft -= 196 // Sola doğru kaydırma işlemi.
+  })
+})
+
+rightArrows.forEach((rightArrow, i) => {
+  rightArrow.addEventListener("click", function () {
+    const containerWidth = movieLists[i].offsetWidth
+    const contentWidth = movieLists[i].scrollWidth
+    const maxScrollLeft = contentWidth - containerWidth + 196
+
+    if (movieLists[i].scrollLeft >= maxScrollLeft) {
+      movieLists[i].scrollLeft = 0 // Listenin sonuna gelindiğinde başa dön.
+    } else {
+      movieLists[i].scrollLeft += 196 // Normal kaydırma işlemi.
+    }
+  })
+})
